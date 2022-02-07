e130cc42ce27d08618561f840f926c80be3a2169
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-02-08 00:06:52 +0300

    routing for node js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const posts = require('./server/routes/posts');
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/posts', posts);
+
+app.get('*' (req,res)=>{
+  res.sendFile(path.join(__dirname,'dist/index.html'))
+});
+
+app.listen(4600 , (req, res))
