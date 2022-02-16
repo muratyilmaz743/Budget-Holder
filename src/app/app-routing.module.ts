80c891ae71049256ca8819405ac49cc5c6dd99f8
Murat-Yilmaz <mrt.yilmaz743@gmail.com>
2022-02-16 21:04:16 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AddingPageComponent} from './adding-page/adding-page.component';
 import {DataTableComponent} from "./data-table/data-table.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 const routes: Routes = [
   {path: 'add', component: AddingPageComponent},
-  {path: '', component: DataTableComponent}
+  {path: 'budget', component: DataTableComponent},
+  {path: 'register',component: RegisterPageComponent},
+  {path: '', component: LandingPageComponent}
 ];
 
 @NgModule({
